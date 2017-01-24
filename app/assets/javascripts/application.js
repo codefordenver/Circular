30a7d59abb3cf85c2842b8d0ef2d4f6f2888678c
Tommasina <tommasina1@gmail.com>
2017-01-23 20:54:21 -0700

    [WIP] Adds gems for google map integration:, getting error message ActionController::RoutingError (No route matches [GET] /assets/bootstrap.min.css.map): when rendering home page. We think that it's maybe to do with the order of scripts in the application layout... any ideas?

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,5 @@
 //= require jquery_ujs
 //= require_tree .
 //= require bootstrap.min
+//= require underscore.min
+// = require gmaps/google
---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,17 @@
+handler = Gmaps.build('Google');
+handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
+  markers = handler.addMarkers([
+    {
+      "lat": 39.7392,
+      "lng": 104.9903,
+      "picture": {
+        "url": "http://people.mozilla.com/~faaborg/files/shiretoko/firefoxIcon/firefox-32.png",
+        "width":  32,
+        "height": 32
+      },
+      "infowindow": "hello!"
+    }
+  ]);
+  handler.bounds.extendWith(markers);
+  handler.fitMapToBounds();
+});
