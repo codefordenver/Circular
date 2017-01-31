b00c0f957dab10b9fb6db7cccd76592a29d86e66
Tommasina <tommasina1@gmail.com>
2017-01-30 19:38:54 -0700

    Adds map to our homepage

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,17 +1,24 @@
-handler = Gmaps.build('Google');
-handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
-  markers = handler.addMarkers([
-    {
-      "lat": 39.7392,
-      "lng": 104.9903,
-      "picture": {
-        "url": "http://people.mozilla.com/~faaborg/files/shiretoko/firefoxIcon/firefox-32.png",
-        "width":  32,
-        "height": 32
-      },
-      "infowindow": "hello!"
-    }
-  ]);
-  handler.bounds.extendWith(markers);
-  handler.fitMapToBounds();
-});
+$(document).ready(function(){
+  initMap();
+})
+
+function initMap(){ 
+  handler = Gmaps.build('Google');
+  handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
+    markers = handler.addMarkers([
+      {
+        "lat": 39.7392,
+        "lng": -104.9903,
+        "picture": {
+          "url": "",
+          "width":  32,
+          "height": 32
+        },
+        "infowindow": "hello!"
+      }
+    ]);
+    handler.bounds.extendWith(markers);
+    handler.fitMapToBounds();
+  });
+}
+  
