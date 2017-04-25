69b181d2974902c1796556d4b6f5ccf787ae99bc
Ann Wolff <annwolffe@gmail.com>
2017-04-24 20:49:20 -0600

    Update buttons to create apartments
    We wired up the buttons on the modal to create apartments when the user
    confirms they want to do so. We also gave a dismiss action to the
    ‘Learn More’ button

---
+++ app/assets/javascripts/create_address.js
@@ -0,0 +1,14 @@
+$(document).ready(() => {
+  $('#create-address').click(() => {
+    event.preventDefault()
+    let streetAddress = $('input[name=apartment]').val()
+    $.ajax({
+      url: '/apartments',
+      method: 'POST',
+      data: {'apartment': {'street_address': streetAddress}}
+    })
+    .done(response => {
+      response()
+    }) 
+  })
+})
