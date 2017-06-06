c577877194e65b185908dd7e65f0d0641eb0c9aa
Ximsce Inconnu <ximsce@gmail.com>
2017-06-05 21:28:50 -0600

    Integrating react-google-maps * adds google map to landing page * centered on Denver with a marker * requires creating a .env.local file with REACT_APP_GOOGLE_MAPS_KEY set to a valid google maps api key

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './navbar';
+import ApartmentMap from './apartmentmap';
 
 class App extends Component {
+  state = {
+    markers: [{
+      position: {
+        lat: 39.7392, lng: -104.9903
+      },
+      key: `Taiwan`,
+      defaultAnimation: 2,
+    }],
+  };
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <NavBar />
+        <ApartmentMap markers={this.state.markers} />
       </div>
     );
   }
