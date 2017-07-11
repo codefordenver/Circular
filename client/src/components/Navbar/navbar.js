2f64b44bb0f32a4b222be0b28ee434c70dfd1e5b
Ximsce Inconnu <ximsce@gmail.com>
2017-07-10 19:08:56 -0600

    applying css from rails app to react app

--- client/src/components/Navbar/navbar.js
+++ client/src/components/Navbar/navbar.js
@@ -10,15 +10,15 @@ class NavBar extends Component {
 
       <div className="col-md-9 navbar-header">
       <p>
-      <a className="home-link" href="/">DENVER: RE<span className="non-bold">IMAGINE</span></a>
+      <a className="home-link" href="/">Recycling Request Tool</a>
       </p>
       </div>
 
       <div className="col-md-3">
       <ul className="top-nav-list">
 
-  <li>sign in with google</li>
-    <li>sign in with facebook</li>
+{/*  <li>sign in with google</li>
+    <li>sign in with facebook</li>*/}
     </ul>
     </div>
     </div>
