6e687aea714c4a16bae9e41d7b3b12602959beb5
Casey Metz <casey@barbershop.io>
2017-07-15 17:01:03 -0600

    autofocus

--- client/src/components/AutoSuggestInput.js
+++ client/src/components/AutoSuggestInput.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import PlacesAutocomplete, {geocodeByAddress, getLatLng} from 'react-places-autocomplete';
+import React, { Component } from 'react';
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
 import scriptLoader from 'react-async-script-loader';
 
 class AutoSuggestInput extends Component {
@@ -10,30 +10,39 @@ class AutoSuggestInput extends Component {
       isGoogleReady: false,
       googleApiError: false
     };
-    this.onChange = address => this.setState({address, googleApiError: false});
+    this.onChange = address => this.setState({ address, googleApiError: false });
   }
 
   handleSelect(address) {
-    this.setState({address});
+    this.setState({ address });
     geocodeByAddress(this.state.address)
       .then(results => getLatLng(results[0]))
-      .then(latLng => {
+      .then((latLng) => {
         console.log('Success', latLng);
-        this.setState({googleApiError: false})
+        this.setState({ googleApiError: false });
       })
-      .catch(error => this.setState({googleApiError: error}));
+      .catch(error => this.setState({ googleApiError: error }));
   }
 
   render() {
+		    const AutocompleteItem = ({ formattedSuggestion }) => (
+  <div className="Demo__suggestion-item">
+    <i className="fa fa-map-marker Demo__suggestion-icon" />
+    <strong>{formattedSuggestion.mainText}</strong>{' '}
+    <small className="text-muted">{formattedSuggestion.secondaryText}</small>
+  </div>);
     const inputProps = {
+      type: 'text',
       value: this.state.address,
-      onChange: this.onChange
+      onChange: this.onChange,
+      autoFocus: true,
+      placeholder: 'Search your address'
     };
 
     return (
       <form>
         { this.state.googleApiError &&
-        <p style={{'color': 'red'}}> Sorry, we're having trouble finding that address </p>}
+        <p style={{ color: 'red' }}> Sorry, we're having trouble finding that address </p>}
         { this.props.isScriptLoaded ?
           <PlacesAutocomplete
             inputProps={inputProps}
@@ -41,7 +50,7 @@ class AutoSuggestInput extends Component {
             onEnterKeyDown={address => this.handleSelect(address)}
             clearItemsOnError
           />
-          : <input type="text"/> }
+          : <input type="text" /> }
       </form>
     );
   }
