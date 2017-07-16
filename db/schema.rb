23a13b95c47437d61f1c3767e8bd47326f5dc945
Tommasina <tommasina.miller@granicus.com>
2017-07-15 22:51:42 -0600

    Adds migration for apartment campaign name to apartments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170716003140) do
+ActiveRecord::Schema.define(version: 20170716014615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170716003140) do
     t.datetime "updated_at",     null: false
     t.float    "lng"
     t.float    "lat"
+    t.string   "name"
   end
 
   create_table "petitions", force: :cascade do |t|
