f80473573826ea91c5073653562f473ede43addc
Casey Metz <casey@barbershop.io>
2017-07-15 20:02:26 -0600

    cta layout on mobile

--- client/src/components/SearchAddress.js
+++ client/src/components/SearchAddress.js
@@ -6,7 +6,8 @@ class SearchAddress extends Component {
   render() {
     return (
       <div className="searchAddress">
-        <h1 className="searchAddress-heading">Does your apartment building need recycling? Build Support!</h1>
+        <h1 className="searchAddress-heading">Need recycling at your building?</h1>
+        <h2 className="searchAddress_sub_heading"> Join or create a campaign!</h2>
         <AutoSuggestInput className="searchAddress-input" />
         <a className="searchAddress-link" href="/">Learn more first</a>
       </div>
