835c6b161afdeccd3fcf7aa23b5e1e4e8ffb7666
Tommasina <tommasina.miller@granicus.com>
2017-07-15 18:39:33 -0600

    Updates apartment specs and factory to reflect 'lng' instead of 'long'

--- spec/factories/apartment.rb
+++ spec/factories/apartment.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :apartment do
     street_address { Faker::Address.street_address }
     lat { rand(-90.000000000...90.000000000) }
-    long { rand(-180.000000000...180.000000000) }
+    lng { rand(-180.000000000...180.000000000) }
   end
 end
