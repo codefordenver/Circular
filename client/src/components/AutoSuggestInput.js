733ca0f8b98848e2535d66c20ce25981e497e953
Casey Metz <casey@barbershop.io>
2017-07-17 09:32:56 -0600

    styling

--- client/src/components/AutoSuggestInput.js
+++ client/src/components/AutoSuggestInput.js
@@ -8,7 +8,8 @@ class AutoSuggestInput extends Component {
     this.state = {
       address: '',
       isGoogleReady: false,
-      googleApiError: false
+      googleApiError: false,
+      isFieldActive: false
     };
     this.onChange = address => this.setState({ address, googleApiError: false });
   }
@@ -26,15 +27,16 @@ class AutoSuggestInput extends Component {
 
   render() {
     const cssClasses = {
-      root: 'form-group',
+      root: 'form_group',
       input: 'search_input',
       autocompleteContainer: 'autocomplete_container'
     };
+
     const AutocompleteItem = ({ formattedSuggestion }) => (
       <div className="input_suggestion_item">
         <i className="fa fa-map-marker" />
-        <strong>{formattedSuggestion.mainText}</strong>{' '}
-        <small className="text_muted">{formattedSuggestion.secondaryText}</small>
+        <strong className="suggestion_text_bold">{formattedSuggestion.mainText}</strong>{' '}
+        <small className="suggestion_text_muted">{formattedSuggestion.secondaryText}</small>
       </div>);
 
     const inputProps = {
@@ -46,20 +48,26 @@ class AutoSuggestInput extends Component {
     };
 
     return (
-      <form className="autosuggest_input_form">
+      <div className="autosuggest_input_form">
         { this.state.googleApiError &&
         <p style={{ color: 'red' }}> Sorry, we're having trouble finding that address </p>}
-        { this.props.isScriptLoaded ?
-          <PlacesAutocomplete
-            autocompleteItem={AutocompleteItem}
-            classNames={cssClasses}
-            inputProps={inputProps}
-            onSelect={address => this.handleSelect(address)}
-            onEnterKeyDown={address => this.handleSelect(address)}
-            clearItemsOnError
-          />
+        <div className="input_form">
+          { this.props.isScriptLoaded ?
+            <PlacesAutocomplete
+              autocompleteItem={AutocompleteItem}
+              highlightFirstSuggestion
+              classNames={cssClasses}
+              inputProps={inputProps}
+              onSelect={address => this.handleSelect(address)}
+              onEnterKeyDown={address => this.handleSelect(address)}
+              clearItemsOnError
+            />
           : <input type="text" /> }
-      </form>
+          { this.state.address && <button className="clear_button"><i className="fa fa-times-circle fa-lg" aria-hidden="true" /></button>
+}
+          <button className="search_button" disabled={!this.state.address}><i className="fa fa-search fa-2x" aria-hidden="true" /></button>
+        </div>
+      </div>
     );
   }
 }
