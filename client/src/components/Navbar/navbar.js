b1d77e84bb46a0f19d39651c96a14a9d21cf8245
Casey Metz <casey@barbershop.io>
2017-07-22 21:46:36 -0600

    remove commented-out css

--- client/src/components/Navbar/navbar.js
+++ client/src/components/Navbar/navbar.js
@@ -6,9 +6,22 @@ class NavBar extends Component {
   render() {
     return (
       <nav className="top_nav">
+        <div className="row">
 
+        <div className="col-md-9 navbar-header">
+        <p>
         <a className="home-link" href="/">Recycling Request Tool</a>
+      </p>
+      </div>
 
+        <div className="col-md-3">
+        <ul className="top-nav-list">
+
+        {/*  <li>sign in with google</li>
+    <li>sign in with facebook</li>*/}
+      </ul>
+      </div>
+      </div>
       </nav>
 
     );
@@ -16,11 +29,3 @@ class NavBar extends Component {
 }
 
 export default NavBar;
-
-// <div className="col-md-3">
-//         <ul className="top-nav-list">
-
-//         {/*  <li>sign in with google</li>
-//     <li>sign in with facebook</li>*/}
-//       </ul>
-//       </div>;
