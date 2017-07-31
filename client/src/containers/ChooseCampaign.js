f9a39596f26b64ab55f869e096bb767a91790cda
Tommasina <tommasina.miller@granicus.com>
2017-07-30 18:50:08 -0600

    Adds radio buttons for nearby campaigns, and sets state for selected

--- client/src/containers/ChooseCampaign.js
+++ client/src/containers/ChooseCampaign.js
@@ -2,13 +2,37 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class ChooseCampaign extends Component {
-  renderNearbyCampaigns(nearbyCampaignsArr) {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedOption: null
+    }
+    this.handleOptionChange = this.handleOptionChange.bind(this)
+  }
+
+  handleOptionChange(e) {
+    this.setState({
+      selectedOption: e.target.value
+    });
+  }
+
+  renderNearbyCampaigns(nearbyCampaignsArr, selectedOption, handleOptionChange) {
     return (
-      <ul>
-        {nearbyCampaignsArr.map(c => <li><p>{ c.street_address }</p></li>)}
-      </ul>
+      nearbyCampaignsArr.map(c => (
+        <div className="radio" key={c.street_address}>
+          <label>
+            <input
+              type="radio"
+              value={c.street_address}
+              checked={selectedOption === c.street_address }
+              onChange={(e) => handleOptionChange(e)} />
+            { c.street_address }
+          </label>
+      </div>)
+      )
     );
   }
+
 
   render() {
     const renderNearby = this.renderNearbyCampaigns;
@@ -18,10 +42,21 @@ class ChooseCampaign extends Component {
         { loading && <p> Searching... </p> }
         { !loading && error && error.searchError && <p>{error.userMessage}</p> }
         { loaded && nearbyCampaigns && Array.isArray(nearbyCampaigns) &&
-        <div>
-          <p>{"Here's some nearby campaigns:"}</p>
-          {renderNearby(nearbyCampaigns)}
-        </div>
+        <form>
+          <p>{"We found these campaigns near you."}</p>
+          <p>{"Do any of these campaigns represent where you live?"}</p>
+          {renderNearby(nearbyCampaigns, this.state.selectedOption, this.handleOptionChange)}
+          <div className="radio" key="no-match">
+            <label>
+              <input
+                type="radio"
+                value="no-match"
+                checked={this.state.selectedOption === "no-match"}
+                onChange={(e) => this.handleOptionChange(e)} />
+              { "None of these match my address." }
+            </label>
+          </div>
+        </form>
         }
         { !loading && nearbyCampaigns && nearbyCampaigns.status === 'okay' && !nearbyCampaigns.results &&
         <p>{"We didn't find any campaigns near you. Would you like to start one?"}</p>
