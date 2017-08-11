c572f889fa77030ac4d7d45144132c20e821d526
yojoecool <bjp0008@auburn.edu>
2017-08-10 19:43:09 -0600

    Creating apartment seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+apartments = Apartment.create([
+  { lat: 39.7392, lng: -104.673828, name: 'Nuggets' },
+  { lat: 39.739163, lng: -104.920209, name: 'Rockies' },
+  { lat: 39.721756, lng: -105.044840, name: 'Avalanche' },
+  { lat: 39.696614, lng: -104.982272, name: 'Cherry Creek' },
+  { lat: 39.659113, lng: -104.865747, name: 'Ariel' },
+  { lat: 39.792355, lng: -105.016302, name: 'Berekely' },
+  { lat: 39.712282, lng: -104.996709, name: 'Baker' },
+  { lat: 39.797109, lng: -104.902870, name: 'Northfield' },
+  { lat: 39.709109, lng: -104.889465, name: 'Windsor' },
+  { lat: 39.748008, lng: -105.006986, name: 'Pepsi' },
+  { lat: 39.749858, lng: -104.946996, name: 'Monkey' },
+  { lat: 39.770741, lng: -104.901162, name: 'Stapleton' },
+  { lat: 39.793815, lng: -104.977670, name: 'North Washington' }
+])
