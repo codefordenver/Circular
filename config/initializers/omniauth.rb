54b1a3162d7a242d7c7f585d35c33f77799192ee
Tommasina <tommasina.miller@granicus.com>
2017-08-14 20:42:22 -0600

    Adds back omniauth for Facebook and google

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,8 +2,8 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, 
-    Rails.application.secrets.google_client_id,
-    Rails.application.secrets.google_app_secret, #, {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+    ENV['google_client_id'],
+    ENV['google_app_secret'], #, {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
   {
     :name => "google_oauth2",
     :scope => "email, profile, plus.me",#, http://gdata.youtube.com",
@@ -15,8 +15,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 
   #Contact facebook, pull keys from 'congfig/secrets.yml', obtain user information.
   provider :facebook,
-    Rails.application.secrets.facebook_app_id,
-    Rails.application.secrets.facebook_app_secret,
+    ENV['facebook_app_id'],
+    ENV['facebook_app_secret']
   {
     :name => "facebook",
     :scope => "email, public_profile",
