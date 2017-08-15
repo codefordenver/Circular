0026142d0c91eec628bf8c55b08c9dabaf6e1f8b
yojoecool <bjp0008@auburn.edu>
2017-08-14 18:56:31 -0600

    Adding campaigns near each other

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,5 +18,10 @@ Campaign.create!([
   { street_address: 'Auraria, Denver, CO, USA', lat: 39.748008, lng: -105.006986, name: 'Pepsi', vote_count: 10 },
   { street_address: 'City Park, Denver, CO, USA', lat: 39.749858, lng: -104.946996, name: 'Monkey', vote_count: 11 },
   { street_address: '3870 Quebec St, Denver, CO 80207, USA', lat: 39.770741, lng: -104.901162, name: 'Stapleton', vote_count: 6 },
-  { street_address: 'North Washington, CO, USA', lat: 39.793815, lng: -104.977670, name: 'North Washington', vote_count: 15 }
+  { street_address: 'North Washington, CO, USA', lat: 39.793815, lng: -104.977670, name: 'North Washington', vote_count: 15 },
+  { street_address: '1330 Pearl St, Denver, CO, USA', lat: 39.737543, lng: -104.979444, name: 'Near1', vote_count: 10 },
+  { street_address: '1280 Pearl St, Denver, CO, USA', lat: 39.736650, lng: -104.979489, name: 'Near2', vote_count: 12 },
+  { street_address: '1200 Pearl St, Denver, CO, USA', lat: 39.735421, lng: -104.979489, name: 'Near3', vote_count: 17 },
+  { street_address: '1218 N Washington St, Denver, CO, USA', lat: 39.735434, lng: -104.978501, name: 'Near4', vote_count: 5 },
+  { street_address: '1153 N Clarkson St, Denver, CO, USA', lat: 39.734649, lng: -104.978057, name: 'Near5', vote_count: 9 },
 ])
