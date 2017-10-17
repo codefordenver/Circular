eac163e3ce30d0d20acd55915fbedd9fe241a7dc
Tommasina <tommasina.miller@granicus.com>
2017-10-16 21:45:15 -0600

    Adds user_id and campaign_id to data to post, Removes debugger

--- client/src/redux/actions/signature.js
+++ client/src/redux/actions/signature.js
@@ -1,11 +1,11 @@
 import { browserHistory } from 'react-router';
 import createApiRequest from '../../utils/createApiRequest';
 
-export function addSignatureToCampaign() {
-  debugger;
+export function addSignatureToCampaign(userId, campaignId) {
+  let data = { user_id: userId, campaign_id: campaignId }
   return {
     type: 'ADD_SIGNATURE_TO_CAMPAIGN',
-    promise: createApiRequest('signatures', 'POST')
+    promise: createApiRequest('signatures', 'POST', data)
   };
 }
 
