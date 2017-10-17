18be461632641a9f2c705fa114552bb9dfd4c043
Tommasina <tommasina.miller@granicus.com>
2017-10-16 20:35:05 -0600

    Adds signatures endpoint for creating the join record between a user and campaign

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get 'campaigns/find', to: 'campaigns#find'
       resources :campaigns
+      post 'signatures', to: 'signatures#create'
     end
   end	
 end
