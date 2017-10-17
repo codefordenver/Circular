2748745ac3473c33845b23b81420cf4f899b32b7
Tommasina <tommasina.miller@granicus.com>
2017-10-16 20:34:01 -0600

    Renames SignPetition to SignCampaign; adds userId and campaignId to post to signatures

---
+++ client/src/components/SignCampaign.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {OAuthSignInButton} from "redux-auth/default-theme";
+import {addSignatureToCampaign} from '../redux/actions/signature'
+// import { Link } from 'react-router';
+
+const SignCampaign = (props) => {
+  const { auth } = props;
+  const { activeCampaign } = props;
+  const user = auth && auth.user;
+  // const isSignedIn = user && user.isSignedIn;
+  const name = user && user.attributes && user.attributes.name;
+  const email = user && user.attributes && user.attributes.email;
+  const userId = user && user.attributes && user.attributes.id;
+  const campaignId = activeCampaign && activeCampaign.campaign && activeCampaign.campaign.id;
+  const sendUserSignature = () => { addSignatureToCampaign(userId, campaignId)};
+  return (
+    <div className="sign-campaign-wrapper">
+      <h2>Show your support!</h2>
+      <OAuthSignInButton next={() => { sendUserSignature()
+      }} provider="google">Google</OAuthSignInButton>
+      <OAuthSignInButton provider="facebook">Facebook</OAuthSignInButton>
+    </div>
+  )
+};
+
+export default connect(
+  ({auth, activeCampaign}) => ({auth, activeCampaign}), {addSignatureToCampaign})(SignCampaign);
