f5202665fbfbe41f32b3efec3763330f0e6f2501
Tommasina <tommasina.miller@granicus.com>
2017-10-16 20:34:42 -0600

    Creates Signatures as join table between user and campaigns
    (I named it petitions at first and then per good Rails migration
    workflow renamed with a migration)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170808022257) do
+ActiveRecord::Schema.define(version: 20171017020322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20170808022257) do
     t.integer  "vote_count",     default: 1
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+  end
+
+  create_table "signatures", id: false, force: :cascade do |t|
+    t.integer "user_id",     null: false
+    t.integer "campaign_id", null: false
+    t.index ["campaign_id", "user_id"], name: "index_signatures_on_campaign_id_and_user_id", using: :btree
+    t.index ["user_id", "campaign_id"], name: "index_signatures_on_user_id_and_campaign_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
