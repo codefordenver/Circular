faabdca4738e402774e12fa30106df36e7969a20
Tommasina <tommasina.miller@granicus.com>
2017-11-04 17:04:16 -0600

    Remove google signin from navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,26 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
-import GoogleButton from 'react-google-button';
 
 class Navbar extends Component {
   renderContent() {
-    if (this.props.auth.data === undefined) {
-      return;
-    }
-
-    if (!this.props.auth.data.googleID) {
-      return (
-        <a className="google-oauth-button" href="/auth/google">
-          <GoogleButton />
-        </a>
-      );
-    }
-    return (
-      <a className="google-oauth-button" href="/api/logout">
-        <GoogleButton label="Sign Out" />
-      </a>
-    );
   }
   render() {
     return (
