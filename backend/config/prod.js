f7db250d9956651dea61de7b07f6800abe299d57
dannyradden <dradden@gmail.com>
2017-11-04 14:29:47 -0600

    update mongodb key name in backend

--- backend/config/prod.js
+++ backend/config/prod.js
@@ -1,7 +1,7 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI,
+  mongoURI: process.env.MONGODB_URI,
   cookieKey: process.env.COOKIE_KEY,
   redirectDomain: process.env.REDIRECT_DOMAIN
 };
