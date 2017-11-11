015b4f9243b93540121b15166f47c58b835c271d
Casey Metz <casey@barbershop.io>
2017-11-11 13:06:59 -0700

    format settings

--- client/src/components/CampaignsMap.js
+++ client/src/components/CampaignsMap.js
@@ -2,34 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router';
 import Modal from 'react-modal';
-import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
-import withScriptjs from 'react-google-maps/lib/async/withScriptjs';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 import * as _ from 'lodash';
 
 // Wrap all `react-google-maps` components with `withGoogleMap` HOC
 // and name it GettingStartedGoogleMap
-const GettingStartedGoogleMap = withRouter(
-  withScriptjs(
-    withGoogleMap(props => (
-      <GoogleMap
-        ref={props.onMapLoad}
-        defaultZoom={12}
-        defaultCenter={{ lat: 39.7392, lng: -104.9903 }}
-        onClick={props.onMapClick}
-      >
-        {props.markers.map(marker => (
-          <Marker
-            key={marker.id}
-            position={{ lat: marker.lat, lng: marker.lng }}
-            onRightClick={() => props.onMarkerRightClick(marker)}
-            onClick={() => props.router.push(`/campaign/${marker.id}`)}
-            title={marker.street_address}
-          />
-        ))}
-      </GoogleMap>
-    ))
-  )
-);
+const GettingStartedGoogleMap = withRouter(withScriptjs(withGoogleMap(props => (
+  <GoogleMap
+    ref={props.onMapLoad}
+    defaultZoom={12}
+    defaultCenter={{ lat: 39.7392, lng: -104.9903 }}
+    onClick={props.onMapClick}
+  >
+    {props.markers.map(marker => (
+      <Marker
+        key={marker.id}
+        position={{ lat: marker.lat, lng: marker.lng }}
+        onRightClick={() => props.onMarkerRightClick(marker)}
+        onClick={() => props.router.push(`/campaign/${marker.id}`)}
+        title={marker.street_address}
+      />
+				))}
+  </GoogleMap>
+))));
 
 const mapUrl = `https://maps.googleapis.com/maps/api/js?v=3.exp&key=${process.env
   .REACT_APP_GOOGLE_MAPS_KEY}`;
@@ -37,7 +32,7 @@ const mapUrl = `https://maps.googleapis.com/maps/api/js?v=3.exp&key=${process.en
 const ApartmentMap = props => (
   <Modal isOpen={props.isOpen} contentLabel="Modal">
     <button className="close_map_button" onClick={props.closeMap}>
-      X
+			X
     </button>
     <GettingStartedGoogleMap
       googleMapURL={mapUrl}
@@ -53,7 +48,7 @@ const ApartmentMap = props => (
                 }}
               /> */}
         </div>
-      }
+			}
       containerElement={<div style={{ height: '50vh' }} />}
       mapElement={<div style={{ height: '100%' }} />}
       onMapLoad={_.noop}
--- client/src/components/SignCampaign.js
+++ client/src/components/SignCampaign.js
@@ -4,9 +4,10 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import SocialLogin from 'react-social-login';
 import Checkbox from './SignatureCheckbox';
-// import GoogleButton from 'react-google-button';
 import { addSignatureToCampaign, logSignerOut } from '../redux/actions/signature';
 // import { beginAuth } from '../redux/actions/authorization';
+
+const authId = process.env.REACT_APP_GOOGLE_AUTH_ID;
 
 const Button = ({ children, triggerLogin, ...props }) => (
   <button onClick={triggerLogin} {...props}>
@@ -21,119 +22,120 @@ class SignCampaign extends Component {
     return (
       <div className="error-message">
         {this.props.signatureObj.error && this.props.signatureObj.error.code === 11000
-          ? 'You already have signed this petition!'
-          : null}
+					? 'You already have signed this petition!'
+					: null}
       </div>
     );
   }
 
- componentWillMount = () => {
-   this.selectedCheckboxes = new Set();
- };
+	componentWillMount = () => {
+	  this.selectedCheckboxes = new Set();
+	};
 
- toggleCheckbox = (label) => {
-   if (this.selectedCheckboxes.has(label)) {
-     this.selectedCheckboxes.delete(label);
-   } else {
-     this.selectedCheckboxes.add(label);
-   }
- };
+	toggleCheckbox = (label) => {
+	  if (this.selectedCheckboxes.has(label)) {
+	    this.selectedCheckboxes.delete(label);
+	  } else {
+	    this.selectedCheckboxes.add(label);
+	  }
+	};
 
- handleFormSubmit = async (formSubmitEvent) => {
-   formSubmitEvent.preventDefault();
+	handleFormSubmit = async (formSubmitEvent) => {
+	  formSubmitEvent.preventDefault();
 
-   const campaignId =
-   this.props.activeCampaign &&
-   this.props.activeCampaign.campaign &&
-   this.props.activeCampaign.campaign._id;
+	  const campaignId =
+			this.props.activeCampaign &&
+			this.props.activeCampaign.campaign &&
+			this.props.activeCampaign.campaign._id;
 
-   // if (this.props.auth === undefined) {
-   //   return <div />;
-   // } // I'm not sure this is needed but I don't remember what it was for...
+	  // if (this.props.auth === undefined) {
+	  //   return <div />;
+	  // } // I'm not sure this is needed but I don't remember what it was for...
 
-   await this.props.addSignatureToCampaign(
-     this.props.auth._id,
-     this.selectedCheckboxes,
-     campaignId
-   );
+	  await this.props.addSignatureToCampaign(
+	    this.props.auth._id,
+	    this.selectedCheckboxes,
+	    campaignId
+	  );
 
-   for (const checkbox of this.selectedCheckboxes) {
-     console.log(checkbox, 'is selected.');
-   }
-   this.props.logSignerOut();
- };
+	  for (const checkbox of this.selectedCheckboxes) {
+	    console.log(checkbox, 'is selected.');
+	  }
+	  this.props.logSignerOut();
+	};
 
- createCheckbox = label => (
-   <Checkbox label={label} handleCheckboxChange={this.toggleCheckbox} key={label} />
- );
+	createCheckbox = label => (
+  <Checkbox label={label} handleCheckboxChange={this.toggleCheckbox} key={label} />
+	);
 
- createCheckboxes = () => {
-   const checkboxes = ['Keep me updated on the status of this request'];
-   return checkboxes.map(label => this.createCheckbox(label));
- };
+	createCheckboxes = () => {
+	  const checkboxes = ['Keep me updated on the status of this request'];
+	  return checkboxes.map(label => this.createCheckbox(label));
+	};
 
- handleSocialLogin = (user) => {
-   console.log(user);
- };
+	handleSocialLogin = (user) => {
+	  console.log(user);
+	};
 
- handleSocialLoginFailure = (err) => {
-   console.error(err);
- };
+	handleSocialLoginFailure = (err) => {
+	  console.error(err);
+	};
 
- checkSignIn = () => {
-   if (this.props.auth && !this.props.auth.googleID) {
-     return (
-       <SocialButton
-         provider="google"
-         appId={'AIzaSyAwICoMBInIKadYG6gXwsfSaL4fGNbog3U'}
-         onLoginSuccess={this.handleSocialLogin}
-         onLoginFailure={this.handleSocialLoginFailure}
-       >
-         {'sign with the googs'}
-       </SocialButton>
-     );
-   }
- };
+	// checkSignIn = () => {
+	// 	if (this.props.auth && !this.props.auth.googleID) {
+	// 		return (
+	// 			<SocialButton
+	// 				provider="google"
+	// 				appId={`${authId}`}
+	// 				onLoginSuccess={this.handleSocialLogin}
+	// 				onLoginFailure={this.handleSocialLoginFailure}
+	// 			>
+	// 				{"sign with the googs"}
+	// 			</SocialButton>
+	// 		);
+	// 	}
+	// };
 
- renderContent() {
-   return (
-     <div className="container">
-       <div className="row">
-         <div className="col-sm-12">
-           {this.createCheckboxes()}
-           <SocialButton
-             provider="google"
-             appId={'45736087042-kdq1atpud65dca2a44hmtpadura3qcfa.apps.googleusercontent.com'}
-             onLoginSuccess={this.handleSocialLogin}
-             onLoginFailure={this.handleSocialLoginFailure}
-           >
-             {'sign with the googs'}
-           </SocialButton>
-         </div>
-       </div>
-     </div>
-   );
- }
+	// renderContent() {
+	// 	return (
+	// 		<div className="container">
+	// 			<div className="row">
+	// 				<div className="col-sm-12">
+	// 					{this.createCheckboxes()}
+	// 					<SocialButton
+	// 						provider="google"
+	// 						appId={googleAuthId}
+	// 						onLoginSuccess={this.handleSocialLogin}
+	// 						onLoginFailure={this.handleSocialLoginFailure}
+	// 					>
+	// 						{"sign with the googs"}
+	// 					</SocialButton>
+	// 				</div>
+	// 			</div>
+	// 		</div>
+	// 	);
+	// }
 
- render() {
-   return (
-     <div className="sign-campaign-wrapper">
-       <h1>Yes, I Want Recycling!</h1>
-       {this.renderError()}
-       <div className="sign-campaign-signature-button">
-         <div>Sign with:</div>
-         <SocialButton
-           provider="google"
-           appId={'45736087042-kdq1atpud65dca2a44hmtpadura3qcfa.apps.googleusercontent.com'}
-           onLoginSuccess={this.handleSocialLogin}
-           onLoginFailure={this.handleSocialLoginFailure}
-         >
-           {'sign with the googs'}
-         </SocialButton>
-       </div>
-     </div>
-   );
- }
+	render() {
+	  console.log(authId);
+	  return (
+  <div className="sign-campaign-wrapper">
+    <h1>Yes, I Want Recycling!</h1>
+    {this.renderError()}
+    <div className="sign-campaign-signature-button">
+      <div>Sign with:</div>
+      <SocialButton
+        provider="google"
+        appId={`${authId}`}
+        onLoginSuccess={this.handleSocialLogin}
+        onLoginFailure={this.handleSocialLoginFailure}
+      >
+        {'sign with the googs'}
+      </SocialButton>
+    </div>
+  </div>
+	  );
+	}
 }
 
 SignCampaign.defaultProps = {
