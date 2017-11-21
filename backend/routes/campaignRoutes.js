2a9795dc0f830667ba248689c629f3888e7a3fad
Danny Radden <dradden@gmail.com>
2017-11-20 18:31:57 -0700

    setup up linter and prettier so that they work together (#238)
    * set up prettier and prettier-eslint
    * add pre-commit hook

--- backend/routes/campaignRoutes.js
+++ backend/routes/campaignRoutes.js
@@ -4,7 +4,10 @@ const Campaign = mongoose.model('Campaign');
 module.exports = app => {
   app.get('/api/campaigns/find', async (req, res) => {
     await Campaign.geoNear(
-      { type: 'Point', coordinates: [parseFloat(req.query.lng), parseFloat(req.query.lat)] },
+      {
+        type: 'Point',
+        coordinates: [parseFloat(req.query.lng), parseFloat(req.query.lat)]
+      },
       {
         spherical: true,
         maxDistance: 200
@@ -13,14 +16,16 @@ module.exports = app => {
         if (err) {
           res.status(422).send(err);
         } else {
-          const data = results.map(({ obj: { address, name, voteCount, _id } }) => {
-            return {
-              address,
-              name,
-              voteCount,
-              id: _id
-            };
-          });
+          const data = results.map(
+            ({ obj: { address, name, voteCount, _id } }) => {
+              return {
+                address,
+                name,
+                voteCount,
+                id: _id
+              };
+            }
+          );
           res.send(data);
         }
       }
@@ -30,16 +35,18 @@ module.exports = app => {
   app.get('/api/campaigns', async (req, res) => {
     const campaigns = await Campaign.find({});
 
-    const data = campaigns.map(({ _id, address, name, voteCount, latLng: { coordinates } }) => {
-      return {
-        address,
-        name,
-        voteCount,
-        lat: coordinates[1],
-        lng: coordinates[0],
-        id: _id
-      };
-    });
+    const data = campaigns.map(
+      ({ _id, address, name, voteCount, latLng: { coordinates } }) => {
+        return {
+          address,
+          name,
+          voteCount,
+          lat: coordinates[1],
+          lng: coordinates[0],
+          id: _id
+        };
+      }
+    );
 
     res.send(data);
   });
