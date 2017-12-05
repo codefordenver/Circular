fc6fa65f30e087426fc63c314bb49d0ce181f213
Willy <willygdouglas@gmail.com>
2017-12-04 20:35:24 -0700

    Cypress initial sample

---
+++ cypress/integration/simple_spec.js
@@ -0,0 +1,20 @@
+describe('The landing page', () => {
+  cy.visit('http://localhost:3000')
+
+  it('prompts the user with about recycling', () => {
+    cy.contains('Need recycling at your building?')
+  })
+
+  describe('when searching an address', () => {
+    it('navigates to the choose campaign page', () => {
+      cy.get('.search_input')
+        .type('Denver')
+        .should('have.value', 'Denver')
+
+      cy.get('.search_button')
+        .click()
+
+      cy.url().should('include', '/choose-campaign')
+    })
+  })
+})
