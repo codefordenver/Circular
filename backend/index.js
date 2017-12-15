ae77b4de0e3d08a65089c9635f6d22a0c88850fc
Laurie Linz <laura.linz@ca.com>
2017-12-14 20:37:27 -0700

    Add in memory mongo for end to end tests

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,28 @@ require('./models/Signature');
 
 require('./services/passport');
 
-mongoose.connect(keys.mongoURI);
+if (process.env.NODE_ENV === 'test') {
+  const MongoInMemory = require('mongo-in-memory');
+
+  var port = 8000;
+  var mongoServerInstance = new MongoInMemory(port); //DEFAULT PORT is 27017
+
+  mongoServerInstance.start((error, config) => {
+    if (error) {
+      console.error(error);
+    } else {
+      //callback when server has started successfully
+
+      console.log('HOST ' + config.host);
+      console.log('PORT ' + config.port);
+
+      var mongouri = mongoServerInstance.getMongouri('myDatabaseName');
+      mongoose.connect(mongouri);
+    }
+  });
+} else {
+  mongoose.connect(keys.mongoURI);
+}
 
 const app = express();
 
--- cypress/integration/simple_spec.js
+++ cypress/integration/simple_spec.js
@@ -1,23 +1,19 @@
 describe('The landing page', () => {
   beforeEach(function() {
-    cy.visit('http://localhost:3000')
-  })
+    cy.visit('http://localhost:3000');
+  });
   it('prompts the user with about recycling', () => {
-    cy.contains('Need recycling at your building?')
-  })
+    cy.contains('Need recycling at your building?');
+  });
 
   describe('when searching an address', () => {
     it('navigates to the choose campaign page', () => {
-      cy.get('.search_input')
+      cy
+        .get('.search_input')
         .type('Denver')
-        .should('have.value', 'Denver')
-      cy.get('.search_button')
-        .click()
-      cy.url().should('include', '/choose-campaign')
-    })
-  })
-})
-
-describe('Create a new campaign', () => {
-  
+        .should('have.value', 'Denver');
+      cy.get('.search_button').click();
+      cy.url().should('include', '/choose-campaign');
+    });
+  });
 });
