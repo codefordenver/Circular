37c13f0e7e94dc86a4b0a5abec2bdbeb146ec4db
Danny Radden <dradden@gmail.com>
2018-01-22 18:31:26 -0700

    Improve persist stage organization and performance (#248)

--- client/src/redux/configureStore.js
+++ client/src/redux/configureStore.js
@@ -3,13 +3,15 @@ import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
 import apiMiddleware from './middleware/api';
-
 import rootReducer from './reducers/index';
+import { loadState } from './localStorage';
 
 export default function configureStore() {
+  const persistedState = loadState();
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   const store = createStore(
     rootReducer,
+    persistedState,
     composeEnhancers(applyMiddleware(logger, thunk, apiMiddleware()))
   );
 
