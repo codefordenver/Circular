182cd29891159a22cb5045038aec3e6eaa6117e4
Ryan Brooks <the.ryan.brooks@gmail.com>
2018-02-05 20:09:34 -0700

    connect to #194 links for navbar (#260)
     styled navbar and navbar linked pages with basic information

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,18 +1,51 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router';
 
-const Navbar = () => (
-  <nav className="navbar">
-    <div className="container">
-      <div className="row">
-        <div className="col-md-9 navbar-header">
-          <Link className="home-link" to="/">
-            <h1>Recycling Request Tool</h1>
-          </Link>
+const Navbar = (props) => {
+  let homeText;
+  if (props.location.pathname === '/') {
+    homeText = 'RE:IMAGINE DENVER';
+  } else {
+    homeText = 'HOME';
+  }
+
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="row">
+          <p>
+            <Link className="home-link" to="/">
+              {homeText}
+            </Link>
+          </p>
+          <ul className="top-nav-list">
+            <li>
+              <Link className="fa" to="/about">
+                Why
+              </Link>
+            </li>
+            <li>
+              <Link className="fa" to="/tools">
+                Tools
+              </Link>
+            </li>
+            <li>
+              <Link className="fa" to="/who-are-we">
+                Who Are We
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
-    </div>
-  </nav>
-);
+    </nav>
+  );
+};
+
+Navbar.propTypes = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired
+  }).isRequired
+};
 
 export default Navbar;
