037c4931b30cb5c3d17c6a3526ca7d73df86ad05
Matt <mhooper72@gmail.com>
2018-02-23 12:13:27 -0700

    added react-bootstrap

--- backend/services/passport.js
+++ backend/services/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
@@ -32,6 +33,28 @@ passport.use(
         googleID: profile.id,
         name: profile.displayName,
         email: profile.emails[0].value
+      }).save();
+      done(null, user);
+    }
+  )
+);
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: keys.facebookAppId,
+      clientSecret: keys.facebookAppSecret,
+      callbackURL: '/auth/facebook/callback'
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ facebookID: profile.id });
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+      const user = await new User({
+        facebookID: profile.id,
+        name: profile.displayName,
+        email: profile.email
       }).save();
       done(null, user);
     }
