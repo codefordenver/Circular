7e4080d51f7898d0a5b00f48491b42d334d877aa
TheBrooks <the.ryan.brooks@gmail.com>
2018-03-02 11:29:17 -0700

    styled the recycling info page

--- client/src/components/RecyclingInfo.js
+++ client/src/components/RecyclingInfo.js
@@ -1,21 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { Grid, Row, Col, PageHeader } from 'react-bootstrap';
 
 const RecyclingInfo = () => (
-  <div className="info_wrapper">
-    <div className="info_header">
-      Denver's recycling rate is only 20%, while the national average is 35%.
-    </div>
-    <div className="info_details">
-      The City of Denver only provides recycling service to single-family residential homes and
-      buildings with seven or fewer units. Furthermore, multi-family building managers are not
-      mandated to provide recycling service, but you and your neighbors can request recycling
-      service from your landlord today!
-    </div>
-    <Link to="/property-owner-faq">
-      <div className="info_footer">Learn More</div>
-    </Link>
+  <Grid fluid>
+    <Row>
+      <Col xs={10} xsOffset={1} md={6} mdOffset={3}>
+        <Row>
+          <Col xs={12}>
+            <PageHeader className="step-by-step-page-header">Wait, But Why?</PageHeader>
+          </Col>
+          <Col xs={12}>
+            <h3 className="step-title">
+              Denver's recycling rate is only 20%, while the national average is 35%.
+            </h3>
+            <p className="step-description">
+              The City of Denver only provides recycling service to single-family residential homes
+              and buildings with seven or fewer units. Furthermore, multi-family building managers
+              are not mandated to provide recycling service, but you and your neighbors can request
+              recycling service from your landlord today!
+            </p>
+          </Col>
+          <Col xs={12}>
+            <Link to="/property-owner-faq" className="info_footer">
+              Learn More
+            </Link>
+          </Col>
+        </Row>
+      </Col>
+    </Row>
+  </Grid>
+  /*
+
+
   </div>
+  */
 );
 
 export default RecyclingInfo;
--- client/src/components/StepByStep.js
+++ client/src/components/StepByStep.js
@@ -3,15 +3,13 @@ import PropTypes from 'prop-types';
 import { Grid, Row, Col, PageHeader } from 'react-bootstrap';
 
 const Step = props => (
-  <Row className="step-container">
-    <Col xs={1} className="step-emoji-icon">
+  <Col xs={12} className="step-container">
+    <div className="step-emoji-icon">
       <p>{props.emojiIcon}</p>
-    </Col>
-    <Col xs={11}>
-      <h3 className="step-title">{props.title}</h3>
-      <p className="step-description">{props.description}</p>
-    </Col>
-  </Row>
+    </div>
+    <h3 className="step-title">{props.title}</h3>
+    <p className="step-description">{props.description}</p>
+  </Col>
 );
 Step.propTypes = {
   emojiIcon: PropTypes.string.isRequired,
@@ -20,39 +18,37 @@ Step.propTypes = {
 };
 
 const StepByStep = () => (
-  <div>
-    <Grid fluid>
-      <Row>
-        <Col xs={10} xsOffset={1} md={6} mdOffset={3}>
-          <Row>
-            <Col xs={11} xsOffset={1} className="page-header-container">
-              <PageHeader className="step-by-step-page-header">How does this work?</PageHeader>
-            </Col>
-            <Step
-              emojiIcon="&#x270D;"
-              title="Create a campaign"
-              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi."
-            />
-            <Step
-              emojiIcon="&#x1F91D;"
-              title="Recruit support"
-              description="ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat."
-            />
-            <Step
-              emojiIcon="&#x1F64F;"
-              title="Request"
-              description="nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            />
-            <Step
-              emojiIcon="&#x1F64C;"
-              title="Recycle!"
-              description="ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam!"
-            />
-          </Row>
-        </Col>
-      </Row>
-    </Grid>
-  </div>
+  <Grid fluid>
+    <Row>
+      <Col xs={10} xsOffset={1} md={6} mdOffset={3}>
+        <Row>
+          <Col xs={12}>
+            <PageHeader className="step-by-step-page-header">How does this work?</PageHeader>
+          </Col>
+          <Step
+            emojiIcon="&#x270D;"
+            title="Create a campaign"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi."
+          />
+          <Step
+            emojiIcon="&#x1F91D;"
+            title="Recruit support"
+            description="ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat."
+          />
+          <Step
+            emojiIcon="&#x1F64F;"
+            title="Request"
+            description="nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          />
+          <Step
+            emojiIcon="&#x1F64C;"
+            title="Recycle!"
+            description="ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam!"
+          />
+        </Row>
+      </Col>
+    </Row>
+  </Grid>
 );
 
 export default StepByStep;
