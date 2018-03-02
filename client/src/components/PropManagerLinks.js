6270e8660d3af0b9bd4055bb74a6bafb7bf9b8c1
TheBrooks <the.ryan.brooks@gmail.com>
2018-03-02 16:06:55 -0700

    structured and styled pulled in homepage components

--- client/src/components/PropManagerLinks.js
+++ client/src/components/PropManagerLinks.js
@@ -1,23 +1,37 @@
 import React from 'react';
+import { Row, Col, PageHeader } from 'react-bootstrap';
 import { Link } from 'react-router';
 
 const PropManagerLinks = () => (
-  <div className="managerlinks_wrapper">
-    <div className="managerlinks_header">Property Managers</div>
-    <div className="managerlinks_details">
-      Provide sustainable waste management to your tenants!<br />
-      Increase tenant satisfaction and boost your building's appeal by providing recycling service.
-      <br />
-      <br />
-      Please visit the page below to estimate your required level of service and tips on purchasing
-      service.
-    </div>
-    <div className="managerlinks_links">
-      <Link to="/denver-recycling-info">
-        <div className="managerlink">Tips And Resources</div>
-        <div className="arrowlink" />
-      </Link>
-    </div>
+  <div>
+    <Col xs={10} xsOffset={1} md={6} mdOffset={0}>
+      <Row>
+        <Col xs={12}>
+          <PageHeader className="step-by-step-page-header">
+            I'm a Property Manager, what can I do?
+          </PageHeader>
+        </Col>
+        <Col xs={12} className="step-title">
+          <h3>Provide sustainable waste management to your tenants! </h3>
+        </Col>
+        <Col xs={12} className="step-description">
+          <p>
+            Increase tenant satisfaction and boost your building's appeal by providing recycling
+            service.
+          </p>
+          <br />
+          <p>
+            Please visit the page below to estimate your required level of service and tips on
+            purchasing service.
+          </p>
+        </Col>
+        <Col xs={12}>
+          <Link to="/denver-recycling-info" className="info-footer">
+            Tips ad Resources
+          </Link>
+        </Col>
+      </Row>
+    </Col>
   </div>
 );
 
--- client/src/components/RecyclingInfo.js
+++ client/src/components/RecyclingInfo.js
@@ -1,40 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router';
-import { Grid, Row, Col, PageHeader } from 'react-bootstrap';
+import { Row, Col, PageHeader } from 'react-bootstrap';
 
 const RecyclingInfo = () => (
-  <Grid fluid>
-    <Row>
-      <Col xs={10} xsOffset={1} md={6} mdOffset={3}>
-        <Row>
-          <Col xs={12}>
-            <PageHeader className="step-by-step-page-header">Wait, But Why?</PageHeader>
-          </Col>
-          <Col xs={12}>
-            <h3 className="step-title">
-              Denver's recycling rate is only 20%, while the national average is 35%.
-            </h3>
-            <p className="step-description">
-              The City of Denver only provides recycling service to single-family residential homes
-              and buildings with seven or fewer units. Furthermore, multi-family building managers
-              are not mandated to provide recycling service, but you and your neighbors can request
-              recycling service from your landlord today!
-            </p>
-          </Col>
-          <Col xs={12}>
-            <Link to="/property-owner-faq" className="info_footer">
-              Learn More
-            </Link>
-          </Col>
-        </Row>
-      </Col>
-    </Row>
-  </Grid>
-  /*
-
-
+  <div>
+    <Col xs={10} xsOffset={1} md={6} mdOffset={0}>
+      <Row>
+        <Col xs={12}>
+          <PageHeader className="step-by-step-page-header">Wait, But Why?</PageHeader>
+        </Col>
+        <Col xs={12}>
+          <h3 className="step-title">
+            Denver's recycling rate is only 20%, while the national average is 35%.
+          </h3>
+          <p className="step-description">
+            The City of Denver only provides recycling service to single-family residential homes
+            and buildings with seven or fewer units. Furthermore, multi-family building managers are
+            not mandated to provide recycling service, but you and your neighbors can request
+            recycling service from your landlord today!
+          </p>
+        </Col>
+        <Col xs={12}>
+          <Link to="/property-owner-faq" className="info-footer">
+            Learn More
+          </Link>
+        </Col>
+      </Row>
+    </Col>
   </div>
-  */
 );
 
 export default RecyclingInfo;
