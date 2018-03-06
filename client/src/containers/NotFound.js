136d1708b793b0b5286b232ef7ec4055dd61ead8
TheBrooks <the.ryan.brooks@gmail.com>
2018-03-06 12:34:14 -0700

    created and styled a basic 404 page

--- client/src/containers/NotFound.js
+++ client/src/containers/NotFound.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const NotFound = () => (
-  <div className="app-container">
-    <h1>I am the 404 page</h1>
+  <div className="not-found-container">
+    <div>
+      <h1>You seem to have gotten lost...</h1>
+    </div>
+    <div>
+      <i className="fa fa-4x fa-recycle loading-spinner slow-spin" />
+    </div>
+    <div>
+      <Link className="home-section-link" to="/">
+        Go back to Home Page
+      </Link>
+    </div>
   </div>
 );
 
