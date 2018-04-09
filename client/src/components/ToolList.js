748cbc12ba7541f015386799afe6a3d710d7ccac
TheBrooks <the.ryan.brooks@gmail.com>
2018-04-09 14:19:54 -0600

    ToolList does not need state so turned removed class

--- client/src/components/ToolList.js
+++ client/src/components/ToolList.js
@@ -1,43 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router';
 import CollapsePanel from './CollapsePanel';
 
-class ToolList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      defaultExpanded: false
-    };
-  }
-
-  togglePanelExpanded = () => {
-    this.setState(prevState => ({
-      expanded: !prevState.expanded
-    }));
-  };
-
-  render() {
-    return (
-      <CollapsePanel
-        headingStyle="bg-blue-color text-center"
-        titleText="Helpful tools:"
-        body={
-          <ul className="tool-list-body">
-            <li>
-              <Link className="toolList" to="/tips-for-requesting">
-                Tips for Approaching your Landlord
-              </Link>
-            </li>
-            <li>
-              <Link className="toolList" to="/denver-learn-more">
-                Denver Recycling Facts
-              </Link>
-            </li>
-          </ul>
-        }
-      />
-    );
-  }
-}
+const ToolList = props => (
+  <CollapsePanel
+    headingStyle="bg-blue-color text-center"
+    titleText="Helpful tools:"
+    body={
+      <ul className="tool-list-body">
+        <li>
+          <Link className="toolList" to="/tips-for-requesting">
+            Tips for Approaching your Landlord
+          </Link>
+        </li>
+        <li>
+          <Link className="toolList" to="/denver-learn-more">
+            Denver Recycling Facts
+          </Link>
+        </li>
+      </ul>
+    }
+  />
+);
 
 export default ToolList;
