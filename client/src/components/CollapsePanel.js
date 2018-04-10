387b519ebcb6780f61f82c5357d201d13d15449a
TheBrooks <the.ryan.brooks@gmail.com>
2018-04-09 18:56:09 -0600

    collapse panel toggles open no matter where the header is clicked

--- client/src/components/CollapsePanel.js
+++ client/src/components/CollapsePanel.js
@@ -6,7 +6,7 @@ class CollapsePanel extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      expanded: this.props.defaultExpanded
+      expanded: props.defaultExpanded
     };
   }
 
@@ -19,29 +19,32 @@ class CollapsePanel extends Component {
   render() {
     const { body, titleText } = this.props;
     return (
-      <Panel
-        /*
+      /* eslint-disable */
+      <div onClick={this.togglePanelExpanded}>
+        {/* eslint-enable */}
+        <Panel
+          /*
           bsStyle="remove-default" causes an console error but it
           is the correct way to remove react-bootstrap defaults.
           the console error is react-bootstraps fault.
         */
-        bsStyle="remove-default"
-        className="collapse-panel"
-        onToggle={this.togglePanelExpanded}
-        expanded={this.state.expanded}
-      >
-        <Panel.Toggle className="collapse-panel-toggle">
-          <Panel.Heading className={this.props.headingStyle}>
-            <Panel.Title className="collapse-panel-title" toggle>
-              {titleText}
-              <i className="fa fa-chevron-down float-right" />
-            </Panel.Title>
-          </Panel.Heading>
-        </Panel.Toggle>
-        <Panel.Collapse>
-          <Panel.Body className="text-black ">{body}</Panel.Body>
-        </Panel.Collapse>
-      </Panel>
+          bsStyle="remove-default"
+          className="collapse-panel"
+          expanded={this.state.expanded}
+        >
+          <Panel.Toggle className="collapse-panel-toggle">
+            <Panel.Heading className={this.props.headingStyle}>
+              <Panel.Title className="collapse-panel-title" toggle>
+                {titleText}
+                <i className="fa fa-chevron-down float-right" />
+              </Panel.Title>
+            </Panel.Heading>
+          </Panel.Toggle>
+          <Panel.Collapse>
+            <Panel.Body className="text-black ">{body}</Panel.Body>
+          </Panel.Collapse>
+        </Panel>
+      </div>
     );
   }
 }
