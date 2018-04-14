78affe6b5f4b71521fbac15f0a711081167cee25
Matt Hooper <mhooper72@gmail.com>
2018-04-14 17:45:05 -0600

    Delete test.js

--- backend/config/test.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID: 'fakekey',
-  googleClientSecret: 'fakekey',
-  mongoURI:
-    'mongodb://code_for_denver:cfd123@ds243335.mlab.com:43335/reimagine-dev',
-  cookieKey: 'fakekey',
-  redirectDomain: 'fakekey'
-};
