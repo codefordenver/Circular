a6ce802891bb674c30d35e31ec291c9388af5b1e
Matt <mhooper72@gmail.com>
2018-04-17 12:47:44 -0600

    created 3 new dbs Dev (seeding to follow), Staging, and Prod all hosted with MLAB. Removed the prior test environment with Mongo-In-Memory

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./prod');
-} else if (process.env.NODE_ENV === 'test') {
-  module.exports = require('./test');
-} else {
+} else if (process.env.NODE_ENV === 'dev') {
   module.exports = require('./dev');
+} else {
+  module.exports = require('./staging');
 }
---
+++ backend/config/staging.js
@@ -0,0 +1,11 @@
+module.exports = {
+  googleClientID:
+    '210192712928-ombrmn50j8n43jlugqkmkq65ikhka2rq.apps.googleusercontent.com',
+  googleClientSecret: '-E5a7_jqhHtcyzy3ER6FNwPU',
+  facebookAppId: '1586286208116445',
+  facebookAppSecret: 'f48312f4152f84d637c27e6d12abe924',
+  mongoURI:
+    'mongodb://ReImagineDev:recycle123@ds247699.mlab.com:47699/db_derick',
+  cookieKey: 'gfgfsgqefdsf2fwfreghj3f23fa847yuiwufw',
+  redirectDomain: 'http://localhost:3000'
+};
