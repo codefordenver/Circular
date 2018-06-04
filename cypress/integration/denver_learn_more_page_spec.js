1c7399dc7081799dfc067e6bd3665488ffa42dd3
David White <whited75@gmail.com>
2018-06-04 15:54:08 -0600

    Add 'denver learn more page' tests

---
+++ cypress/integration/denver_learn_more_page_spec.js
@@ -0,0 +1,15 @@
+describe('The denver learn more page', () => {
+  beforeEach(function() {
+    cy.visit('http://localhost:3000/denver-learn-more');
+  });
+
+  describe('Why Recycle? toggle panel', () => {
+    it('Toggles panel', () => {
+      cy
+        .get('a.collapse-panel-toggle')
+        .contains('Why Recycle?')
+        .click();
+      cy.get('div.text-black.panel-body').should('be.visible');
+    });
+  });
+});
