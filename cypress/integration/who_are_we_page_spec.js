c788478767d37800c6fe5e0a4c8cd539840e1427
David White <whited75@gmail.com>
2018-06-04 15:09:03 -0600

    Add 'who are we' page tests

---
+++ cypress/integration/who_are_we_page_spec.js
@@ -0,0 +1,26 @@
+describe('The who are we page', () => {
+  beforeEach(function() {
+    cy.visit('http://localhost:3000/who-are-we');
+  });
+  it('Prompts the user with who are we', () => {
+    cy.contains('Who Are We');
+  });
+
+  describe('Code for Denver link', () => {
+    it('Navigates to the Code for Denver website', () => {
+      cy
+        .get('a')
+        .contains('Code For Denver')
+        .should('have.attr', 'href', 'https://www.codefordenver.org/');
+    });
+  });
+
+  describe('Eco-Cycle link', () => {
+    it('Navigates to the Eco Cycle website', () => {
+      cy
+        .get('a')
+        .contains('Eco-Cycle')
+        .should('have.attr', 'href', 'https://www.ecocycle.org');
+    });
+  });
+});
