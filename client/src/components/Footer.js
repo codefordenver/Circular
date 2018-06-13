bd8d70e7d08015eaf86bbc8440382b3ec6d47b91
katiescruggs <katie.e.scruggs@gmail.com>
2018-06-12 20:13:22 -0600

    Restyle footer and add copyright info

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,6 +5,9 @@ import { Link } from 'react-router';
 const Footer = () => (
   <Grid fluid className="footer-container">
     <Col xs={12}>
+      <p>Questions? Reach out to us!</p>
+      <i className="fa fa-copyright" />
+      2018 Eco-Cycle Inc & Code for Denver | All Rights Reserved |
       <Link className="footer-link" to="/privacy-policy">
         Privacy Policy
       </Link>
