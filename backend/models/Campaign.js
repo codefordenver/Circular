99173bcd75341c9e31bbe2aa31028fe16c2c4c54
Dave <daviddwhite75@gmail.com>
2018-06-28 21:09:48 -0600

    Remove campaign name (#377)
    * Remove campaign name
    
    * Clean up unsued Campaign Name references
    
    * Delete name attribute on Campaign model

--- backend/models/Campaign.js
+++ backend/models/Campaign.js
@@ -7,7 +7,6 @@ var SchemaTypes = mongoose.Schema.Types;
 
 const campaignSchema = new Schema(
   {
-    name: { type: String, required: true, unique: true },
     address: { type: String, required: true },
     latLng: { type: SchemaTypes.GeoJSON, required: true },
     voteCount: { type: Number, default: 0 },
