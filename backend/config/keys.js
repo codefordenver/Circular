5c3609619d6703bd5a0a2db7c0ece7bb6b6054bc
hoop71 <mhooper72@gmail.com>
2018-07-26 12:00:25 -0600

    Remove Node Backend

--- backend/config/keys.js
+++
@@ -1,7 +0,0 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./prod');
-} else if (process.env.NODE_ENV === 'test') {
-  module.exports = require('./test');
-} else {
-  module.exports = require('./dev');
-}
