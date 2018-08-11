ceae8310d19b1e73f52be15b660dd45a9bce096b
Julie Dursema <jmdursema@gmail.com>
2018-08-11 16:44:00 -0600

    Style welcome modal

--- client/src/containers/WelcomeModal.js
+++ client/src/containers/WelcomeModal.js
@@ -10,42 +10,52 @@ class WelcomeModal extends Component {
   }
   render() {
     return (
-      <div className="welcome-modal">
-        <div className="welcome-header">
-          <h1>WELCOME!</h1>
-          <h2>Want recyling?</h2>
-          {this.props.activeCampaign.campaign ? (
-            <h2>Sign the petition to bring it to{this.props.activeCampaign.campaign.address}</h2>
-          ) : (
-            ''
-          )}
+      <div className="welcome-modal-wrapper">
+        <div className="exit-welcome-modal">X</div>
+        <div className="welcome-modal-main">
+          <div className="welcome-header">
+            <h1>WELCOME!</h1>
+            <h2>Want recyling?</h2>
+            {this.props.activeCampaign.campaign ? (
+              <h2>
+                Sign the petition to bring it to<br />
+                {this.props.activeCampaign.campaign.address}
+              </h2>
+            ) : (
+              ''
+            )}
+          </div>
+          <div className="sign-with-box">
+            <h3>SIGN WITH:</h3>
+            <button className="google-btn">GOOGLE</button>
+            <h4>OR</h4>
+            <button className="facebook-btn">FACEBOOK</button>
+            <h4>OR</h4>
+            <form className="user-email-form">
+              <label htmlFor="first-name-input">First Name:</label>
+              <input id="first-name-input" />
+              <label htmlFor="last-name-input">Last Name:</label>
+              <input id="last-name-input" />
+              <label htmlFor="email-input">Email:</label>
+              <input id="email-input" />
+              <div className="checkbox">
+                <input type="checkbox" id="status-request-input" checked />
+                <label htmlFor="status-request-input">
+                  Keep me updated on the status of the request
+                </label>
+              </div>
+              <div className="checkbox">
+                <input type="checkbox" id="public-display-input" checked />
+                <label htmlFor="public-display-input">
+                  Publicly display my first name on this page
+                </label>
+              </div>
+              <p>By signing, you accept the Terms of Service and Privacy Policy.</p>
+              <button className="submit-btn">SUBMIT</button>
+            </form>
+          </div>
+          <button>No thanks, I'll sign later -></button>
         </div>
-        <div className="sign-with-box">
-          <h3>SIGN WITH:</h3>
-          <button className="google-btn">GOOGLE</button>
-          <h3>OR</h3>
-          <button className="facebook-btn">FACEBOOK</button>
-          <h3>OR</h3>
-          <form>
-            <label htmlFor="first-name-input">First Name:</label>
-            <input id="first-name-input" />
-            <label htmlFor="last-name-input">Last Name:</label>
-            <input id="last-name-input" />
-            <label htmlFor="email-input">Email:</label>
-            <input id="email-input" />
-            <input type="checkbox" id="status-request-input" checked />
-            <label htmlFor="status-request-input">
-              Keep me updated on the status of the request
-            </label>
-            <input type="checkbox" id="public-display-input" checked />
-            <label htmlFor="public-display-input">
-              Publicly display my first name on this page
-            </label>
-            <p>By signing, you accept the Terms of Service and Privacy Policy.</p>
-            <button className="submit-btn">SUBMIT</button>
-          </form>
-        </div>
-        <button>No thanks, I'll sign later -></button>
       </div>
     );
   }
