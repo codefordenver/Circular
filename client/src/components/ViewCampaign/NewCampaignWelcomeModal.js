07352cb94897120d804e2bfd91b4f69ad18672c1
hoop71 <mhooper72@gmail.com>
2018-08-15 16:01:36 -0600

    Update inline styles

--- client/src/components/ViewCampaign/NewCampaignWelcomeModal.js
+++ client/src/components/ViewCampaign/NewCampaignWelcomeModal.js
@@ -8,65 +8,66 @@ const btnStyle = {
   outline: 'none'
 };
 
-const NewCampaignWelcomeModal = ({ onHide, show }) => (
-  // if (!show) {
-  //   return null;
-  // }
-
-  <div>
-    <Modal keyboard show onHide={onHide}>
-      <Modal.Header closeButton>
-        <Modal.Title>You've activated your campaign!</Modal.Title>
-      </Modal.Header>
-      <Modal.Body className="New-Campaign-Welcome-Modal-Wrapper">
-        <h4 id="welcome-modal-header">Here are some pro tips</h4>
-        <section style={{ marginLeft: '1em' }}>
-          <p id="emoji-span">
-            <span role="img" aria-label="right arrow">
-              ➡️
-            </span>{' '}
-            Sign the campaign!
-          </p>
-          <p>
-            <span role="img" aria-label="thumbs up">
-              👍
-            </span>{' '}
-            <a
-              href={`${process.env.PUBLIC_URL}/flyer.pdf`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Print the YES WE CAN Flier
-            </a>
-          </p>
-          <p>
-            <span role="img" aria-label="clipboard">
-              📝
-            </span>{' '}
-            <a
-              href={`${process.env.PUBLIC_URL}/CollectSignatures.pdf`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Gather signatures manually
-            </a>
-          </p>
-          <p>
-            <span role="img" aria-label="social share">
-              📲
-            </span>{' '}
-            Share via social media
-          </p>
-        </section>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button style={btnStyle} onClick={onHide}>
-          Go To My Campaign!
-        </Button>
-      </Modal.Footer>
-    </Modal>
-  </div>
-);
+const NewCampaignWelcomeModal = ({ onHide, show }) => {
+  if (!show) {
+    return null;
+  }
+  return (
+    <div>
+      <Modal keyboard show={show} onHide={onHide}>
+        <Modal.Header closeButton>
+          <Modal.Title style={{ color: 'black' }}>You've activated your campaign!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body style={{ color: 'black' }}>
+          <h4 style={{ color: 'black' }}>Here are some pro tips</h4>
+          <section style={{ marginLeft: '1em' }}>
+            <p>
+              <span role="img" aria-label="right arrow">
+                ➡️
+              </span>{' '}
+              Sign the campaign!
+            </p>
+            <p>
+              <span role="img" aria-label="thumbs up">
+                👍
+              </span>{' '}
+              <a
+                href={`${process.env.PUBLIC_URL}/flyer.pdf`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Print the YES WE CAN Flier
+              </a>
+            </p>
+            <p>
+              <span role="img" aria-label="clipboard">
+                📝
+              </span>{' '}
+              <a
+                href={`${process.env.PUBLIC_URL}/CollectSignatures.pdf`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Gather signatures manually
+              </a>
+            </p>
+            <p>
+              <span role="img" aria-label="social share">
+                📲
+              </span>{' '}
+              Share via social media
+            </p>
+          </section>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button style={btnStyle} onClick={onHide}>
+            Go To My Campaign!
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+};
 
 NewCampaignWelcomeModal.propTypes = {
   onHide: PropTypes.func.isRequired,
