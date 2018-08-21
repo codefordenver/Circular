f615903bcf163bb68d1320cdc23e908525e0e655
hoop71 <mhooper72@gmail.com>
2018-08-21 16:19:16 -0600

    add travis keys

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,15 +3,12 @@ import 'firebase/auth';
 import 'firebase/database';
 import 'firebase/firestore';
 
-console.log(process.env);
 // FIREBASE KEYS IMPORT BASED ON ENV
 firebase.initializeApp({
   apiKey: `${process.env.REACT_APP_apiKey}`,
   authDomain: `${process.env.REACT_APP_authDomain}`,
   databaseURL: `${process.env.REACT_APP_databaseURL}`,
-  projectId: `${process.env.REACT_APP_projectId}`,
-  storageBucket: `${process.env.REACT_APP_storageBucket}`,
-  messagingSenderId: `${process.env.REACT_APP_messagingSenderId}`
+  projectId: `${process.env.REACT_APP_projectId}`
 });
 
 export const firestore = firebase.firestore();
