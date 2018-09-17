353294b312ef04fe2e93ad139669d8dcf9c8f264
Julian Arellano <julianarellano9@gmail.com>
2018-09-17 14:06:40 -0600

    WIP: Steps component (without animations)

--- client/src/components/HowItWorks/HowItWorks.js
+++ client/src/components/HowItWorks/HowItWorks.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Grid, Row, Col, Image } from 'react-bootstrap';
 import AutoSuggestInput from '../AutoSuggestInput';
-import SectionSlice from './SectionSlice';
-import '../../stylesheets/components/_HowItWorks.scss';
-import Footer from '../Footer/Footer';
+// import SectionSlice from './SectionSlice';
+// import '../../stylesheets/components/_HowItWorks.scss';
+
 import screenShot from '../../images/screen-shot.png';
 import list from '../../images/list.png';
 import letter from '../../images/letter.png';
 import smallList from '../../images/small-list.png';
 import guideline1 from '../../images/guildline1.png';
 import guideline2 from '../../images/guideline2.png';
+
+import Steps from './Steps';
+import Footer from '../Footer/Footer';
 
 const HowItWorks = () => (
   <div className="how-it-works-container">
@@ -20,204 +23,134 @@ const HowItWorks = () => (
         </h1>
       </div>
     </div>
-    <div className="how-it-works-content-section">
-      <Grid>
-        {/* ----------- SECTION 1 ------------*/}
-        <Row>
-          <Col sm={12} md={2}>
-            <div className="how-it-works-logo">
-              <center>
-                <i className="fa fa-bullhorn" style={{ width: '100%', fontSize: '2em' }} />
-              </center>
-              <h1> 1. Create a Campaign </h1>
-            </div>
-          </Col>
-          <Col className="slices-container" sm={12} md={4}>
-            <SectionSlice
-              header="Share your campaign"
-              description="Print a flier, post in a public place, like laundry or mail room, or share via social media"
-            />
-            <SectionSlice header="Time Feature" description="You can have extra time if you need" />
-            <SectionSlice
-              header="Tools"
-              description="Access resources like recycling guidelines, tip for approaching your lanloard, data documentation and more"
-            />
-          </Col>
-          <Col sm={12} md={6} style={{ marginTop: '2em' }}>
-            <SectionSlice
-              center
-              header="Explore"
-              description="Click on the map to explore other campaigns"
-            />
-            <Image src={screenShot} responsive />
-          </Col>
-        </Row>
-        {/* ----------- SECTION 2 ------------*/}
-        <Row
-          style={{ marginTop: '3em', marginRight: 0, marginLeft: 0 }}
-          className="how-it-works-content-section-grey"
-        >
-          <Col sm={12} md={2}>
-            <div className="how-it-works-logo-grey">
-              <center>
-                <i className="fa fa-users blue-color" style={{ width: '100%', fontSize: '2em' }} />
-              </center>
-              <h1 className="blue-color"> 2. RECRUIT SUPPORT FROM NEIGHBOBRS </h1>
-            </div>
-          </Col>
-          <Col sm={12} md={10}>
-            <Row sm={12} md={12}>
-              <h3 className="blue-color" style={{ textAlign: 'center', fontWeight: 500 }}>
-                This will let your lanloard know others support recycling in your building, too! You
-                are not alone! Signing will also show your lanloard people pledge to use recycling
-                services correctly.
-              </h3>
-            </Row>
-            <Row sm={12} md={12}>
-              <Col sm={12} md={5}>
-                <div className="top-spacing">
-                  <SectionSlice
-                    center
-                    color
-                    header="Gather Signatures"
-                    description="Post this is a public place so that neighbors can sign the petition with their unit number"
-                  />
-                </div>
-              </Col>
-              <Col sm={12} md={2}>
-                <Image style={{ marginTop: '2em' }} src={list} responsive />
-              </Col>
-              <Col sm={12} md={5}>
-                <div className="top-spacing">
-                  <SectionSlice
-                    color
-                    center
-                    header="Get the word out!"
-                    description="Go door to door to let your neighbors know, host a community meeting, announce this at your next HOA meeting, host a recycling party!"
-                  />
-                </div>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-        {/* ----------- SECTION 3------------*/}
-        <div style={{ marginBottom: '2em' }}>
-          <Row className="how-it-works-content-section">
-            <Col sm={12} md={2}>
-              <div className="how-it-works-logo">
-                <center>
-                  <i className="fa fa-comment" style={{ width: '100%', fontSize: '2em' }} />
-                </center>
-                <h1> 3. Request Recycling From lanloard </h1>
+
+    <Steps>
+      {/* ----------- SECTION 1 ------------*/}
+      <Row>
+        <Col xs={12} md={10} mdOffset={1}>
+          <Row>
+            <Col xs={12}>
+              <div className="step-icon-and-title-container">
+                <i className="fa fa-bullhorn how-icon" />
+                <h2>1. Create or Join a Campaign</h2>
               </div>
             </Col>
-            <Col sm={12} md={10}>
-              <Col sm={12} md={6}>
-                <Row size={12}>
-                  <div className="top-spacing">
-                    <SectionSlice
-                      center
-                      header="Now you have signatures"
-                      description="Submit the letter to your landloard along with the petition signatures. If possible bring other neighbors along, there is strength in numbers."
-                    />
-                  </div>
-                </Row>
-                <Row sm={12}>
-                  <Col sm={5}>
-                    <Image src={letter} responsive />
-                  </Col>
-                  <Col sm={1} style={{ marginTop: '2em' }}>
-                    <h2 style={{ fontSize: 70 }}>
-                      <center>+</center>
-                    </h2>
-                  </Col>
-                  <Col sm={5}>
-                    <Image style={{ marginTop: '2em' }} src={smallList} responsive />
-                  </Col>
-                </Row>
-              </Col>
 
-              <Col sm={12} md={6}>
-                <Row>
-                  <div className="top-spacing">
-                    <SectionSlice
-                      center
-                      header="Now, it's up to them"
-                      description="It my take them one week or longer to get beck to you. They may have to check with thier current waste provider to request quotes for recycling services."
-                    />
-                  </div>
-                </Row>
-                <Row sm={12}>
-                  <center>
-                    <div>
-                      <h2> Did you get service?</h2>
-                    </div>
-                  </center>
-                </Row>
-                <Row sm={12}>
-                  <Col sm={6}>
-                    <div className="top-spacing">
-                      <SectionSlice center header="Yes" description="Congraduations!" />
-                    </div>
-                  </Col>
-                  <Col sm={6}>
-                    <div className="top-spacing">
-                      <SectionSlice
-                        center
-                        header="No"
-                        description="Tell us why. We want to gather data on why landloard deny their tenants service."
-                      />
-                    </div>
-                  </Col>
-                </Row>
-              </Col>
+            <Col xs={12}>
+              <div className="">
+                <h3>Discover Your Building's Campaign Status</h3>
+                <p>
+                  Enter your apartment building's address in the search bar below (or on our
+                  homepage) to find out if there is a recycling campaign already active in your
+                  building. Then, follow the steps to create a brand new campaign or sign the online
+                  petition for an existing one.
+                </p>
+                <AutoSuggestInput />
+              </div>
             </Col>
           </Row>
-        </div>
-        {/* ----------- SECTION 4 ------------*/}
-        <Row className="how-it-works-content-section-grey">
-          <Col sm={12} md={2}>
-            <div className="how-it-works-logo-grey" style={{ paddingTop: '1em' }}>
-              <center>
-                <i
-                  className="fa fa-recycle blue-color"
-                  style={{ width: '100%', fontSize: '2em' }}
-                />
-              </center>
-              <h1 className="blue-color"> 4. Recycle </h1>
-            </div>
-          </Col>
-          <Col sm={12} md={5}>
-            <div className="top-spacing">
-              <SectionSlice
-                color
-                center
-                header="Recycling Guidelines"
-                description={`Congraduations for getting recycling! Best practice shows that posting guildelines will help your neighbors recycle correctly. ${'\n'} ${'\n'}
-                    When a recycling bin is too contaminated your complex will be either given 1 week to clean out the recycling bin or will be charged extra to take it to a landfill as trash.
-                    Common contaminants in the recycing bin include plastic bags because they jamb up the machine and dispable ware such as red solo cups, disposable cutlery and disposable plates.`}
-              />
-            </div>
-          </Col>
-          <Col sm={12} md={5}>
-            <Col sm={12} md={6}>
-              <Image src={guideline2} responsive />
+        </Col>
+      </Row>
+
+      {/* ----------- SECTION 2 ------------*/}
+      <Row>
+        <Col xs={12} md={10} mdOffset={1}>
+          <Row>
+            <Col xs={12}>
+              <div className="step-icon-and-title-container">
+                <i className="fa fa-users how-icon" />
+                <h2>2. Recruit Your Neighbors</h2>
+              </div>
             </Col>
-            <Col sm={12} md={6}>
-              <Image src={guideline1} responsive />
+
+            <Col xs={12}>
+              <div className="">
+                <h3>Power in Numbers</h3>
+                <p>
+                  Now it's time to spread the word about your building's new recycling campaign to
+                  your neighbors. Gathering signatures from your fellow tenants let's your landlord
+                  know just how important recycling services are to your community!{' '}
+                  {/* Each signature also shows a tenant's pledge to 
+                                        make <em> proper </em> use of any provided recycling services. */}
+                </p>
+              </div>
             </Col>
-          </Col>
-        </Row>
-        {/* ----------- SECTION 5 ------------*/}
-        <Row style={{ marginBottom: '2em' }}>
-          <center>
-            <h1>Ready? Active your campaign! </h1>
-          </center>
-          <AutoSuggestInput />
-        </Row>
-      </Grid>
-      <Footer />
-    </div>
+
+            <Col xs={12}>
+              <div className="">
+                <h3>Tools</h3>
+                <ul>
+                  <li>
+                    Print and post this petition in a public space in your apartment building, such
+                    as a laundry or mail room.
+                    <a
+                      className=""
+                      href={`${process.env.PUBLIC_URL}/flyer.pdf`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {' '}
+                      SignatureSheet.pdf
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      {/* ----------- SECTION 3 ------------*/}
+      <Row>
+        <Col xs={12} md={10} mdOffset={1}>
+          <Row>
+            <Col xs={12}>
+              <div className="step-icon-and-title-container">
+                <i className="fa fa-comment how-icon" />
+                <h2>3. Request Recyling From Your Landlord</h2>
+              </div>
+            </Col>
+
+            <Col xs={12}>
+              <div className="">
+                <p>
+                  Submit the letter to your landlord along with the petition signatures. If
+                  possible, bring other neighbors along; there is great strength in numbers.
+                </p>
+              </div>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+
+      {/* ----------- SECTION 4 ------------*/}
+      <Row>
+        <Col xs={12} md={10} mdOffset={1}>
+          <Row>
+            <Col xs={12}>
+              <div className="step-icon-and-title-container">
+                <i className="fa fa-recycle how-icon" />
+                <h2>4. Recycle!</h2>
+              </div>
+            </Col>
+
+            <Col xs={12}>
+              <div className="">
+                <p>
+                  Congraduations for getting recycling services for your building! Best practice
+                  shows that posting guildelines will help your neighbors recycle correctly. When a
+                  recycling bin is too contaminated your complex will either be given 1 week to
+                  clean out the recycling bin or be charged extra to take it to a landfill as trash.
+                  Common contaminants in the recycing bin include plastic bags because they jam up
+                  the machine and disposable ware such as red solo cups, disposable cutlery and
+                  disposable plates.
+                </p>
+              </div>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Steps>
+    <Footer />
   </div>
 );
 
---
+++ client/src/components/HowItWorks/Steps.js
@@ -0,0 +1,134 @@
+import React, { Component } from 'react';
+import { Row, Col } from 'react-bootstrap';
+// import { Transition, animated } from 'react-spring';
+
+class Steps extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedStep: props.selectedStep || 0
+    };
+  }
+
+  _goToStep(i) {
+    this.setState({ selectedStep: i });
+  }
+
+  _nextStep() {
+    if (this.state.selectedStep + 1 < this.props.children.length) {
+      this.setState({ selectedStep: this.state.selectedStep + 1 });
+    }
+  }
+
+  _prevStep() {
+    if (this.state.selectedStep - 1 > -1) {
+      this.setState({ selectedStep: this.state.selectedStep - 1 });
+    }
+  }
+
+  _renderStepSelectors() {
+    const numSteps = this.props.children.length;
+    const selectedStep = this.state.selectedStep;
+    const a = [];
+    a.push(<div className={'half-spacer'} key={'halfSpacer-start'} />);
+    for (let i = 0; i < numSteps; i++) {
+      let stepSelectorClasses = 'step-selector';
+      let stepSpacerClasses = 'spacer-line';
+      if (selectedStep === i) {
+        stepSelectorClasses += ' selected';
+      } else {
+        stepSelectorClasses += ' not-selected';
+      }
+      if (i < selectedStep) {
+        stepSelectorClasses += ' highlighted';
+        stepSpacerClasses += ' highlighted';
+      }
+
+      const selector = (
+        <div
+          className={stepSelectorClasses}
+          key={`step-selector-${i}`}
+          onClick={() => {
+            this._goToStep(i);
+          }}
+        >
+          <div className="circle">{i + 1}</div>
+        </div>
+      );
+      a.push(selector);
+
+      if (i !== numSteps - 1) {
+        const stepSpacerLine = <div className={stepSpacerClasses} key={`step-spacer-${i}`} />;
+        a.push(stepSpacerLine);
+      }
+    }
+    a.push(<div className={'half-spacer'} key={'halfSpacer-end'} />);
+    return a;
+  }
+
+  _renderChildren() {
+    const wrappedChildren = [];
+    for (let i = 0; i < this.props.children.length; i++) {
+      const wrappedChild = (
+        <div className="single-step-content" key={`step-content-${i}`}>
+          <div className="container">{this.props.children[i]}</div>
+        </div>
+      );
+      wrappedChildren.push(wrappedChild);
+    }
+    return wrappedChildren;
+  }
+
+  render() {
+    let prevStepBtnClassNames = 'step-button prev';
+    if (this.state.selectedStep === 0) {
+      prevStepBtnClassNames += ' disabled';
+    }
+
+    let nextStepBtnClassNames = 'step-button next';
+    if (this.state.selectedStep === this.props.children.length - 1) {
+      nextStepBtnClassNames += ' disabled';
+    }
+
+    return (
+      <div className="steps-container">
+        <div className="step-selectors-container">{this._renderStepSelectors()}</div>
+
+        <div
+          className={'steps-content-container'}
+          style={{ left: `-${this.state.selectedStep * 100}%` }}
+        >
+          {this._renderChildren()}
+        </div>
+
+        <div className="container">
+          <Row>
+            <Col xs={12} md={10} mdOffset={1}>
+              <div className="step-end-buttons">
+                <div
+                  className={prevStepBtnClassNames}
+                  onClick={() => {
+                    this._prevStep();
+                  }}
+                >
+                  PREVIOUS
+                </div>
+                <div className="take-space" />
+                <div
+                  className={nextStepBtnClassNames}
+                  onClick={() => {
+                    this._nextStep();
+                  }}
+                >
+                  NEXT
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Steps;
