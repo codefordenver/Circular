0618557f9a3d5a857d17b85181d60408f1ef7f29
Julian Arellano <julianarellano9@gmail.com>
2018-10-21 12:13:33 -0600

    refactored tenant or property manager choice on home page to be its own component, minor style change to buttons, and pointed property manager button link to manager-resources page

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Grid } from 'react-bootstrap';
 import { connect } from 'react-redux';
-import { Link } from 'react-router';
 import PropTypes from 'prop-types';
 import HeroCTA from '../components/HeroCTA';
 // import CampaignsMap from '../components/CampaignsMap';
 import StepByStep from '../components/StepByStep';
-// import InfoAndLinks from '../components/Informational/InfoAndLinks';
+import TenantOrPMChoice from '../components/TenantOrPMChoice';
 import Footer from '../components/Footer/Footer';
 import Banner from '../components/Banner';
+
 import {
   firebaseSearchAddressFlow,
   clearInitialSearchResults
@@ -41,37 +41,7 @@ class Home extends Component {
           </Banner>
         </Grid>
         <StepByStep />
-        <Grid className="call-to-action-section">
-          <Row>
-            <Col xs={12} lg={10} lgOffset={1}>
-              <div>
-                <h2>Ready? Let's improve recycling together!</h2>
-              </div>
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={12} lg={10} lgOffset={1}>
-              <div>
-                <p>
-                  You deserve convenient recycling. Apartment or condo managers are not required to
-                  provide recycling services for residents, but there is something you can do.
-                  Organize your neighbors and request recycling from your buliding manager. Together
-                  we can make Denver a sustainability leader!
-                </p>
-              </div>
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={12}>
-              <div className="buttons-wrapper">
-                <div>
-                  <Link to="how-does-this-work">I'm a tenant</Link>
-                  <Link to="how-does-this-work">I'm a property manager</Link>
-                </div>
-              </div>
-            </Col>
-          </Row>
-        </Grid>
+        <TenantOrPMChoice />
         <Footer />
       </div>
     );
