7fa8fd8d6d65aa179aaf140d6e7b8ef7289c301d
David White <whited75@gmail.com>
2018-12-11 22:13:38 -0500

    HeroCTA component test

--- client/src/components/HeroCTA.test.js
+++ client/src/components/HeroCTA.test.js
@@ -1,5 +1,23 @@
 import React from 'react';
-import HeroCTA from './HeroCTA';
-import renderer from 'react-test-renderer';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-test.skip('HeroCTA component', () => {});
+import HeroCTA from './HeroCTA';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+function setup() {
+  const enzymeWrapper = shallow(<HeroCTA />);
+
+  return {
+    enzymeWrapper
+  };
+}
+
+describe('HeroCTA', () => {
+  const { enzymeWrapper } = setup();
+
+  it('renders self', () => {
+    expect(enzymeWrapper.find('h1').exists()).toBe(true);
+  });
+});
